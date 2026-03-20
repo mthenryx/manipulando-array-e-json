07ddb6ac6cf9dbd31ce191f029b6e7e2a6e994e6
MATHEUS HENRY DOS SANTOS <25204051@CFP127ALUNO.LOCAL>
2026-03-20 16:31:18 -0300

    Desafio de manipulação de ARRAY e JSON concluido com sucesso, funções que retornam o necessario e se não conseguirem retornam falso

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -6,45 +6,119 @@
 * **********************************************************************/
 
 const informacoes = require("./estados_cidades.js")
+const locEstados = informacoes.listaDeEstados.estados
 
 const getListaDeEstados = function(){
-    let locEstados = informacoes.listaDeEstados.estados
-    let estados    = []
-    let lista
+    let estados = []
     let quantidade
-    
+    let resultado
+
     locEstados.forEach(function(uf){
         estados.push(uf.sigla)
     })
 
     quantidade = estados.length
 
-    lista = {estados, quantidade}
-    
-    return lista
+    resultado = {estados, quantidade}
+
+    return resultado
 }
 
 const getDadosEstado = function(sigla){
-    let locEstados = informacoes.listaDeEstados.estados
+    let resultado = false
 
-    console.log(locEstados.sigla)
-    console.log(locEstados.nome)
-    console.log(locEstados.capital)
-    console.log(locEstados.regiao)
+    locEstados.forEach(function(uf){
+        if (uf.sigla == String(sigla).toLocaleUpperCase()){
+            resultado = {
+                "uf" : uf.sigla,
+                "descricao": uf.nome,
+                "capital": uf.capital,
+                "regiao": uf.regiao
+            }
+        }
+    })
+
+    return resultado
 }
 
-const getCapitalEstado = function(){
-    
+const getCapitalEstado = function(sigla){
+    let resultado = false
+
+    locEstados.forEach(function(item){
+        if(item.sigla == String(sigla).toLocaleUpperCase()){
+            resultado = {
+                "uf": item.sigla, "descricao": item.nome, "capital": item.capital
+            }
+        }
+    })
+
+    return resultado
 }
 
-const getEstadoRegiao = function(){
-    
+const getEstadoRegiao = function(regiao){
+    let resultado = {
+        "regiao": regiao.toLocaleUpperCase(),
+        "estados": []
+    }
+
+    for(let estado of locEstados){
+        if(String(regiao).toLocaleUpperCase() == String(estado.regiao).toLocaleUpperCase()){
+            resultado.estados.push({
+                "uf": estado.sigla, "descricao": estado.nome
+            })
+        }
+    }
+
+    if(resultado.estados.length === 0){
+        resultado = false
+    }
+
+    return resultado
 }
 
 const getCapitalPais = function(){
-    
+    let resultado = {
+        "capitais": []
+    }
+
+    locEstados.forEach(function(itemCapital){
+        if (itemCapital.capital_pais){
+            let capital = {
+                "capital_atual": itemCapital.capital_pais.capital,
+                "uf": itemCapital.sigla,
+                "descricao": itemCapital.nome,
+                "capital": itemCapital.capital,
+                "regiao": itemCapital.regiao,
+                "capital_pais_ano_inicio": itemCapital.capital_pais.ano_inicio,
+                "capital_pais_ano_termino": itemCapital.capital_pais.ano_fim
+            }
+
+            resultado.capitais.push(capital)
+        }
+    })
+
+    return resultado
 }
 
-const getCidades = function(){
-    
+const getCidades = function(sigla){
+    let resultado = false
+
+    locEstados.forEach(function(item){
+        if (item.sigla == String(sigla).toLocaleUpperCase()){
+            let cidades = []
+
+            item.cidades.forEach(function (itemCidades){
+                cidades.push(itemCidades.nome)
+            })
+
+            resultado = {
+                "uf": item.sigla,
+                "descricao": item.nome,
+                "quantidade_cidades": item.cidades.length,
+                "cidades": cidades
+            }
+        }
+    })
+
+    return resultado
 }
